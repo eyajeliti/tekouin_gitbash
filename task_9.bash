1c1eee94ac9a1fa93985104bc873c4ace58acf9e
eyajeliti <eyajelliti@gmail.com>
2023-08-03 11:36:44 +0200

    update task

--- task_9.bash
+++ task_9.bash
@@ -6,6 +6,6 @@ while read -r line; do
     GROUP_ID=$(echo "$line" | cut -d ':' -f 4)
     HOME_DIRECTORY=$(echo "$line" | cut -d ':' -f 6)
     COMMAND_SHELL=$(echo '$line' | cut -d ':' -f 7)
-    echo " The user #USERNAME is part of the #GROUP_ID gang, lives in #HOME_dDIRECTORY and rides #COMMAND_SHELL. #USER_ID'S place is protected by the passcode #PASSWORD, more info about the user here : #USER_ID_INFO"
+    echo " The user $USERNAME is part of the $GROUP_ID gang, lives in $HOME_dDIRECTORY and rides $COMMAND_SHELL. $USER_ID's place is protected by the passcode $PASSWORD, more info about the user here : $USER_ID_INFO"
 done < /etc/passwd
 
